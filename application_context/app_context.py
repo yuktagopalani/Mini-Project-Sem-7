a6a710203323d127e2676a5b3432e49d13e40925
Yukta <59793009+yuktagopalani@users.noreply.github.com>
2023-01-11 13:10:49 +0530

    Update app_context.py

--- application_context/app_context.py
+++ application_context/app_context.py
@@ -13,10 +13,7 @@ class AppContext:
     def __init__(self):
         self.sentiment_pipeline = pipeline("sentiment-analysis", model="cardiffnlp/twitter-roberta-base-sentiment")
         self.twitter_search_base_url = "https://api.twitter.com/2/tweets/search/recent?query="
-        self.twitter_search_headers = {
-            'Authorization': 'Bearer AAAAAAAAAAAAAAAAAAAAAKOwiwEAAAAAtKyB0zMcFq0Qpz%2FFgPmbLbE1c9E%3DgW25ZZjYQladyqKCAzRassZLVZvvG7PvHiPhiKVvyo4oAhmDzj',
-            'Cookie': 'guest_id=v1%3A166341311025469413'
-        }
+        
 
 
 app_context = AppContext()
